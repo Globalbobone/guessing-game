80d5b4340b5ab6f3ff54ea234c1b43eb5fc66c2b
Aliaksei Pliakhanau <3253255@rambler.ru>
2017-09-17 21:37:32 +0300

    midify src/guessing-game.js -

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
 
+    constructor() {
+    	this.number = 0;
+    	this.min = 0;
+    	this.max = 0;
+    }
     setRange(min, max) {
-
-    }
-
+			this.min = min - 1;
+			this.max = max + 1;
+	  }
     guess() {
-
+ 			 this.number = Math.round(this.min + (this.max - this.min) / 2); 
+ 			 // this.number = Math.ceil(((this.max + this.min) / 2) * 10 / 10);
+ 			 return this.number;
     }
-
     lower() {
-
+    	this.max = this.number + 1;
     }
-
     greater() {
-
+    	this.min = this.number - 1;
     }
 }
 
